d1a7c140231bcada9262eb4283a2b398fbff4649
hiyu3298 <akamatsu11218991@gmail.com>
2021-04-22 07:22:20 +0000

    [Add] つぶやき投稿機能を追加

--- app/controllers/admins/post_images_controller.rb
+++ app/controllers/admins/post_images_controller.rb
@@ -1,8 +1,16 @@
 class Admins::PostImagesController < ApplicationController
   def new
+    @post_image = PostImage.new
   end
 
   def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.admin_id = current_admin.id
+    if @post_image.save
+      redirect_to admins_post_image_path(@post_image)
+    else
+      render :new
+    end
   end
 
   def index
@@ -10,8 +18,18 @@ class Admins::PostImagesController < ApplicationController
   end
 
   def show
+    @post_image = PostImage.find(params[:id])
   end
 
   def destroy
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to admins_post_images_path
+  end
+
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:practice_name, :image, :tweet, :admin_id)
   end
 end
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,3 +1,4 @@
 class PostImage < ApplicationRecord
-  belongs_to :user
+  belongs_to :admin
+  attachment :image
 end
