491e10429774c222292bcda70d312d8064b1c461
hiyu3298 <akamatsu11218991@gmail.com>
2021-04-26 01:57:53 +0000

    [Add] いいね機能を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_23_054004) do
+ActiveRecord::Schema.define(version: 2021_04_26_014838) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2021_04_23_054004) do
     t.string "genre"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "post_comments", force: :cascade do |t|
